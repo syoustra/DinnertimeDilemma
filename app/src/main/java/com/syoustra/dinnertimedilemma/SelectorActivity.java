d830e6bb2eed1870265459bebcc7a945fa407108
syoustra <steph.you07@gmail.com>
2015-11-21 19:07:34 -0500

    Adding generic second page

--- app/src/main/java/com/syoustra/dinnertimedilemma/SelectorActivity.java
+++ app/src/main/java/com/syoustra/dinnertimedilemma/SelectorActivity.java
@@ -1,51 +1,22 @@
 package com.syoustra.dinnertimedilemma;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button mRandomSelectorButton;
-
+public class SelectorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mRandomSelectorButton = (Button)findViewById(R.id.randomSelectorButton);
-
-        mRandomSelectorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Just don't blame me...", Toast.LENGTH_LONG).show();
-                selectRestaurant();
-            }
-        });
+        setContentView(R.layout.activity_selector);
     }
 
-    private void selectRestaurant() {
-        Intent intent = new Intent(this, SelectorActivity.class);
-        startActivity(intent);
-    }
-
-
-
-
-
-
-
-    // Menu Code Only ... can delete if needed
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_selector, menu);
         return true;
     }
 
@@ -63,5 +34,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    // End deletable menu code
 }
